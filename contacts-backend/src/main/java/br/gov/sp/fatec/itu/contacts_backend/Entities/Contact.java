7aad297b9aa147370cd9ea29a3d45c3355ec5bf2
Nicolas <nick.nogueira02@gmail.com>
2025-07-06 17:18:26 -0300

    fix: added CrossOrigin to ContactsController

--- contacts-backend/src/main/java/br/gov/sp/fatec/itu/contacts_backend/Entities/Contact.java
+++ contacts-backend/src/main/java/br/gov/sp/fatec/itu/contacts_backend/Entities/Contact.java
@@ -18,7 +18,6 @@ public class Contact implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
     private String name;
     private String email;
     private String phone;
--- contacts-backend/src/main/resources/data.sql
+++ contacts-backend/src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO TBL_CONTACTS ( name, email, phone, address, birthday, company, title, category, favorite) 
-VALUES ('Leonardo', 'leonardo@exemplo.com', '12345-6789', '123 Main St', '1990/01/01', 'Fatec Itu', 'vagabundo', 'AMIGO', false);
+VALUES ('Leonardo', 'leonardo@exemplo.com', '12345-6789', '123 Main St', '1990/01/01', 'Fatec Itu', 'brabo', 'AMIGO', false);
 INSERT INTO TBL_CONTACTS ( name, email, phone, address, birthday, company, title, category, favorite) 
 VALUES ('Matheus tramontina', 'matheus@exemplo.com', '98765-4321', '456 Elm St', '1995/05/05', 'Fatec Itu', 'estudante', 'OUTROS', true);
